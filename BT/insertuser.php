0ee02d11ff6b5cce03dfc3b60fb8f93ec33315c1
anh-1234 <loncon0903@gmail.com>
2025-12-17 21:57:00 +0700

    Add files via upload

---
+++ BT/insertuser.php
@@ -0,0 +1,15 @@
+<?php 
+	require_once "db.php"; // Gọi 1 lần
+
+	$uname = isset($_POST['uname']) ? $_POST['uname'] : "";
+	$email = isset($_POST['email']) ? $_POST['email'] : "";
+	
+	$sql = "insert into users (name, email, date_created) values (?, ?, ?)";
+
+	$db = new DbHelper();
+	$last_id = $db->insert($sql, [$uname, $email, date('Y-m-d H:i:s')]);
+	
+	//echo "ID mới được chèn: " . $last_id;
+	header("location: listofusers.php");
+
+?>
